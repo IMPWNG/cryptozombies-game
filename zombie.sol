ce9cbdcc01e1469ebff0a4e9c0ef41468bfbd22f
IMPWNG <matisse.ach+github@gmail.com>
2021-12-28 15:10:52 -0500

    first

---
+++ zombie.sol
@@ -0,0 +1,38 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+contract ZombieFactory {
+
+    event NewZombie(uint zombieId, string name, uint dna);
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+
+    // uint key to store and look up the wombie based on its id and the value an address
+    mapping (uint => address) public zombieToOwner;
+    // key is an address and value a uint for keeping track oh how many zombies an owner has
+    mapping (address => uint) ownerZombieCount;
+
+    function _createZombie(string memory _name, uint _dna) private {
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        // start here
+        emit NewZombie(id, _name, _dna);
+    }
+
+    function _generateRandomDna(string memory _str) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string memory _name) public {
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+
+}
